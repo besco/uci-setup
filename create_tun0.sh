9ecb726a426b26f5baca51817dc7bb0f328a6519
Wasiliy Besedin <besco@yabesco.ru>
2016-08-11 02:11:23 +0300

    Add 15.05 support

--- create_tun0.sh
+++ create_tun0.sh
@@ -2,26 +2,30 @@
 
 createTun() {
     echo " - Create interface"
-    x=`uci set network.tun0=interface  `
-    x=`uci set network.tun0.proto=none `
+    x=`uci set network.tun0=interface`
+    x=`uci set network.tun0.proto=none`
     x=`uci set network.tun0.ifname=tun0`
 }
 
 createFwZone() {
     echo " - Create fw zone"
-    x=`uci add firewall zone                    `
-    echo " - Setup fw zone"
-    x=`uci set firewall.@zone[-1].name=tun0     `
-    x=`uci set firewall.@zone[-1].forward=REJECT`
-    x=`uci set firewall.@zone[-1].output=ACCEPT `
-    x=`uci set firewall.@zone[-1].input=REJECT  `
-    x=`uci set firewall.@zone[-1].network=tun0  `
-    x=`uci set firewall.@zone[-1].masq=1        `
-    x=`uci set firewall.@zone[-1].mtu_fix=1     `
-    echo " - Create forwarding rule"
-    x=`uci add firewall forwarding               `
-    x=`uci set firewall.@forwarding[-1].dest=tun0`
-    x=`uci set firewall.@forwarding[-1].src=lan  `
+    if [ `uci show firewall|grep tun0 -c` -eq 0 ]; then
+        x=`uci add firewall zone`
+        echo " - Setup fw zone"
+        x=`uci set firewall.@zone[-1].name=tun0`
+        x=`uci set firewall.@zone[-1].forward=REJECT`
+        x=`uci set firewall.@zone[-1].output=ACCEPT`
+        x=`uci set firewall.@zone[-1].input=REJECT`
+        x=`uci set firewall.@zone[-1].network=tun0`
+        x=`uci set firewall.@zone[-1].masq=1`
+        x=`uci set firewall.@zone[-1].mtu_fix=1`
+        echo " - Create forwarding rule"
+        x=`uci add firewall forwarding`
+        x=`uci set firewall.@forwarding[-1].dest=tun0`
+        x=`uci set firewall.@forwarding[-1].src=lan`
+    else
+        echo "Zone already exist"
+    fi
 }
 
 commitChanges() {
@@ -31,16 +35,17 @@ commitChanges() {
 
 getFiles() {
     mkdir -p /etc/openvpn
-    x="wget https://main.mob-voip.net/$username/configs/$username.conf --user=$username --password=$password --no-check-certificate -O /etc/openvpn/$username.conf"
-    x="wget https://main.mob-voip.net/$username/keys/$username.crt --user=$username --password=$password --no-check-certificate -O /etc/openvpn/$username.crt"
-    x="wget https://main.mob-voip.net/$username/keys/$username.key --user=$username --password=$password --no-check-certificate -O /etc/openvpn/$username.key"
-    x="wget https://main.mob-voip.net/$username/keys/ca.crt --user=$username --password=$password --no-check-certificate -O /etc/openvpn/ca.crt"
-    x="wget https://main.mob-voip.net/$username/keys/dh2048.pem --user=$username --password=$password --no-check-certificate -O /etc/openvpn/dh2048.pem"
-    x="wget https://main.mob-voip.net/$username/keys/ta.key --user=$username --password=$password --no-check-certificate -O /etc/openvpn/ta.key"
-
+    x=`host main.mob-voip.net`
+    x=`wget https://main.mob-voip.net/$username/configs/$username.conf --user=$username --password=$password --no-check-certificate -O /etc/openvpn/$username.conf`
+    x=`wget https://main.mob-voip.net/$username/keys/$username.crt --user=$username --password=$password --no-check-certificate -O /etc/openvpn/$username.crt`
+    x=`wget https://main.mob-voip.net/$username/keys/$username.key --user=$username --password=$password --no-check-certificate -O /etc/openvpn/$username.key`
+    x=`wget https://main.mob-voip.net/$username/keys/ca.crt --user=$username --password=$password --no-check-certificate -O /etc/openvpn/ca.crt`
+    x=`wget https://main.mob-voip.net/$username/keys/dh2048.pem --user=$username --password=$password --no-check-certificate -O /etc/openvpn/dh2048.pem`
+    x=`wget https://main.mob-voip.net/$username/keys/ta.key --user=$username --password=$password --no-check-certificate -O /etc/openvpn/ta.key`
 }
 
 checkSoft() {
+    echo "Check wget"
     if [ `opkg list-installed|grep wget -c` -eq 0 ]; then
         opkg update
         if [ $? -ne 0 ]; then
@@ -53,9 +58,22 @@ checkSoft() {
             exit 99
         fi
     fi
+    echo "Check openvpn"
+    if [ `opkg list-installed|grep openpvn -c` -eq 0 ]; then
+        version=`cat /etc/openwrt_release|grep RELEASE|awk '{split(\$0,a,"="); print a[2] }'|sed s/\'\$//|sed s/^\'//|awk '{split(\$0,a,".");print a[1]}'`
+        echo $version
+        x=`wget --no-check-certificate https://main.mob-voip.net/_openvpn/$version/openvpn.ipk -O /tmp/openvpn.ipk`
+        opkg install --force-checksum /tmp/openvpn.ipk
+    fi
+
 }
 
-echo test $#
+runVpn() {
+    /etc/init.d/openvpn enable
+    /etc/init.d/openvpn restart
+}
+
+
 while test $# -gt 0
 do
     param1=`echo $1 |awk '{split(\$0,a,"=");print a[1]}'`
@@ -76,16 +94,22 @@ done
 
 if [ -z $username ]; then
     echo "Error. No username."
-    exit(99)
+    echo ""
+    echo "Use $0 --username=<username> --password=<password>"
+    exit 99
 fi
 
 if [ -z $password ]; then
     echo "Error. No password."
-    exit(99)
+    echo ""
+    echo "Use $0 --username=<username> --password=<password>"
+    exit 99
 fi
 
+checkSoft
 getFiles
 
-# createTun
-# createFwZone
-# commitChanges
+createTun
+createFwZone
+commitChanges
+runVpn
